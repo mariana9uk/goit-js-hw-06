a745c45c68768bb4b7fc55fc62b05eeb4784ccb2
mariana9uk <124436149+mariana9uk@users.noreply.github.com>
2023-06-12 23:03:04 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -1,7 +1,5 @@
 const inputEl = document.querySelector('#name-input');
 const outputEl = document.querySelector('#name-output');
 
-inputEl.addEventListener('input', (e) => {
-    outputEl.textContent = e.target.value;
-});
-inputEl.addEventListener('blur', () => { outputEl.textContent = "Anonimus" });
+inputEl.addEventListener('input', (e) =>
+{outputEl.textContent=e.currentTarget.value===""?"Anonimus": e.currentTarget.value});
